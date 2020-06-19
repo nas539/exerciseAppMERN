622002dc23265d259c782353615c7da56e6c7a20
Nicholas Sharp <naas8905@gmail.com>
2020-06-18 18:41:27 -0700

    Setting up MongoDB Connection

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose')
+const port = process.env.PORT || 5000;
+const app = express();
+
+require('dotenv').config();
+
+app.use(express.static(__dirname + '/dist/'));
+app.get(/.*/, function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+})
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+app.listen(port);
+
+
+console.log("server started");
